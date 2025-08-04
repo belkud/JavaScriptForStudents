e60303ae54a594aeb2e6f624c10f4c6ac556e181
belkud <vlad-belkud@yandex.ru>
2025-08-04 20:40:57 +0300

    telephone_write_logics

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,34 @@
 import './style.css'
 
-//! console.log();
+//! Задание с телефонным номером
+let telephone_number = document.querySelector('#telephone_number') as HTMLInputElement
+let clean_number = document.querySelector('#clean_number') as HTMLInputElement
+
+
+telephone_number.addEventListener('keydown',(e:any)=> {
+    let numbers = telephone_number.value.length
+    if (Number(e.key)<=9) {
+        if (numbers>=0 && numbers<=3) {
+            telephone_number.value='+7 ('
+        } else if(numbers==7) {
+            telephone_number.value+= ') '
+        } else if(numbers==12 || numbers==15) {
+            telephone_number.value+='-'
+        }   
+    } else if (e.key !='Backspace') {
+        e.preventDefault()
+    } 
+    // else if (e.key =' ') {
+    //  telephone_number.value+='+'
+    // }
+    // console.log(e);
+    
+})
+
+clean_number.addEventListener('click', (e)=> {
+    e.preventDefault()
+    console.log(e);
+    
+    telephone_number.value=''
+})
+
