7212e40e51aa5ad27f53ce3185a4290e1ae01443
belkud <vlad-belkud@yandex.ru>
2025-08-06 07:36:07 +0300

    list_with_copiraiters

--- src/main.ts
+++ src/main.ts
@@ -26,34 +26,49 @@ clean_number.addEventListener('click', (e)=> {
     telephone_number.value=''
 })
 
+
+
+
+
+
+
+
+//! Реализовать веб-страницу «Каталог копирайтеров».
+
+let user_info = document.querySelectorAll('table tr td:last-child') as any
+
+// try {
     
 
-
-
-
 let users = await fetch('https://jsonplaceholder.typicode.com/users')
-let all_users = document.querySelector('#all_users') as HTMLDivElement
-let user_info = document.querySelector('#user_info') as HTMLDivElement
-
 let user = await users.json()
-console.log(user[0]);
+
+let all_users = document.querySelector('#all_users') as HTMLDivElement
+
 for (let i = 0; i < user.length; i++) {
-    all_users.innerHTML+= `<div class="user_names">${user[i].name}<div>`
+    all_users.innerHTML+= `<div>${user[i].name}<div>`
 }
 
-let el = all_users.children
-console.log(el);
+let users_name = all_users.children
+console.log(users_name);
 
-for (let i = 0; i < el.length; i++) {
-    el[i].addEventListener('click', (e)=> {
-        user_info.innerHTML = JSON.stringify(user[i])
-        console.log(e.target);
-        
+for (let i = 0; i < users_name.length; i++) {
+    users_name[i].addEventListener('click', ()=> {
+
+        user_info[0].innerHTML = `${user[i].name}</div>`
+        user_info[1].innerHTML = `${user[i].username}</div>`
+        user_info[2].innerHTML = `${user[i].address.city + ', ' + user[i].address.street}</div>`
+        user_info[3].innerHTML = `${user[i].email}</div>`
+        user_info[4].innerHTML = `${user[i].phone}</div>`
+        user_info[5].innerHTML = `${user[i].website}</div>`
+        // console.log(e.target);
     })
+    
 }
 
+// } catch (e) {
+    // console.log('Сервер не передал данные');
+       
+// }
 
-// https://jsonplaceholder.typicode.com/users?id=2
-
-//! https://jsonplaceholder.typicode.com/users
 
