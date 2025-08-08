bd08cfa8654a0ef95fd8f3fc7db6d69b71343436
belkud <vlad-belkud@yandex.ru>
2025-08-08 10:00:10 +0300

    test_page

--- src/main.ts
+++ src/main.ts
@@ -48,6 +48,7 @@ try {
     let users = await fetch('https://jsonplaceholder.typicode.com/users')
     let user = await users.json()
     
+  console.log(...user[0] )
     
 for (let i = 0; i < user.length; i++) {
     all_users.innerHTML+= `<div>${user[i].name}<div>`
@@ -68,17 +69,16 @@ for (let i = 0; i < users_name.length; i++) {
         user_number= user[i].id
         console.log(user_number);
         user_posts.innerHTML=''
+        show_posts.innerHTML = `Show ${String(user[i].name).split(' ')[0]}'s posts`
         user_table.style.display = 'block'
         show_posts.style.display = 'block'
     })
 }
 
-
 } catch (e) {
     console.log('Сервер не передал данные');
        all_users.innerHTML+= 'Сервер не передал данные (перезагрузите страницу)'
 }
-   
 }
 
 
@@ -99,10 +99,10 @@ show_posts.addEventListener('click', ()=> {
             console.log(user_post[i]);
             user_posts.innerHTML+= `
             <div class="users_post">
-                <div><b>${user_post[i].id}</b></div><br>
                 <div><b>${user_post[i].title}</b></div><br>
                 <div>${user_post[i].body}</div>
             </div>`
+          
         }
     }
 })  
@@ -116,17 +116,15 @@ show_posts.addEventListener('click', ()=> {
 showUsersPost ()
 
 
+//! Реализовать веб-страницу «Каталог копирайтеров», завершение
 
 
 
 
-
-
-
-// show_posts.scrollIntoView ({
-//     block:'center',
-//     // behavior: 'smooth'
-// })
+show_posts.scrollIntoView ({
+    block:'center',
+    // behavior: 'smooth'
+})
 
 
 
@@ -134,3 +132,32 @@ showUsersPost ()
  
 
 
+// let test = [
+//   {
+//     "userId": 1,
+//     "id": 1,
+//     "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+//     "body": "quia et suscipitsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+//   },
+//   {
+//     "userId": 1,
+//     "id": 2,
+//     "title": "qui est esse",
+//     "body": "est rerum tempore vitaesequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
+//   },
+//   {
+//     "userId": 1,
+//     "id": 3,
+//     "title": "ea molestias quasi exercitationem repellat qui ipsa sit aut",
+//     "body": "et iusto sed quo iurevoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"
+//   },
+//   {
+//     "userId": 1,
+//     "id": 4,
+//     "title": "eum et est occaecati",
+//     "body": "ullam et saepe reiciendis voluptatem adipiscisit amet autem assumenda provident rerum culpa\nquis hic commodi nesciunt rem tenetur doloremque ipsam iure\nquis sunt voluptatem rerum illo velit"
+//   },]
+
+
+// ;
+  
