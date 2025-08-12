48947ced987da84b6ea2aecc808f3afafcf4c995
belkud <vlad-belkud@yandex.ru>
2025-08-12 23:38:07 +0300

    fixed_bugs

--- src/main.ts
+++ src/main.ts
@@ -28,6 +28,99 @@ clean_number.addEventListener('click', (e)=> {
 
 
 
+//!!!!!!!!!!!!!!!!!! задание с поездом!!!!!!!!!!!!!!!!!!!!
+
+let seats = document.querySelector('#seats') as HTMLDivElement
+let tickets_chart = document.querySelector('#tickets_chart') as HTMLDivElement
+let book = document.querySelector('#book') as HTMLInputElement
+let total = document.querySelector('#total') as HTMLDivElement
+
+
+for (let i = 0; i < seats.children.length; i++) {
+  seats.children[i].innerHTML +=`
+      <div>
+      <input type="checkbox">
+      <label></label>
+      </div>
+      <div>
+        <input type="checkbox">
+        <label></label>
+        </div>
+      <div>
+        <input type="checkbox">
+        <label></label>
+        </div>
+      <div>
+      <input type="checkbox">
+        <label></label>
+      </div>
+      `
+      }
+
+
+
+
+let tickets_label = seats.getElementsByTagName('label')
+let tickets_input = seats.getElementsByTagName('input')
+
+for (let i = 0; i < tickets_label.length; i++) {  
+  tickets_input[i].name  = String(i+1)
+  tickets_label[i].innerText  = String(i +1)
+}
+ 
+
+let data = document.querySelector('#data') as HTMLInputElement
+
+let today = new Date()
+data.value = `${today.getFullYear()}-${today.getMonth()+1< 10 ? 0 : ''}${today.getMonth()+1}-${today.getDate()<=9 ? 0 : ''}${today.getDate()}`
+
+
+let directions = document.querySelector('#directions') as HTMLSelectElement
+
+
+let totalAcc = 0 as any
+
+
+book.addEventListener('click', (e)=> {
+  e.preventDefault()
+  
+for (let i = 0; i < tickets_input.length; i++) {
+      if (tickets_input[i].checked == true) {
+  tickets_chart.innerHTML+= `<div>
+  <div>${directions.value}</div>
+  <div>${data.value.split('-').reverse().join('.')}</div>
+  <div>${tickets_input[i].name}</div>
+  </div>`
+  totalAcc+=1*1600
+  console.log('test');
+  console.log(totalAcc);
+  
+    total.innerHTML = `Total: Общая стоимость ${totalAcc} рублей`
+} 
+
+
+
+//! пишем cookie
+document.cookie = `общая стоимость ${totalAcc} долларов; max-age =50`
+
+//! обнуляем Acc
+setTimeout(() => {
+    totalAcc=0  
+
+//! чистим инпуты
+    let inputs = document.querySelectorAll('input')
+    inputs.forEach(el => {
+  el.checked = false; 
+})});
+
+}})
+
+console.log(data.value.split('-').reverse().join('.'));
+
+
+
+//!!!!!!!!!!!!!!!!!! задание с поездом, завершение!!!!!!!!!!!!!!!!!!!!
+
 
 
 
