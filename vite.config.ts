8fbde3abd644d64c5c1f37e4f3f2e57b4522ad46
belkud <vlad-belkud@yandex.ru>
2025-08-13 09:37:24 +0300

    test2

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,4 @@
-import { resolve } from "path";
-
 export default {
-    base: '/JavaScriptForStudents/',
-    build: {
-      rollupOptions: {
-        input: {
-          // @ts-ignore
-          main: resolve(__dirname, 'index.html'),
-          // @ts-ignore
-        //   chat: resolve(__dirname, 'chat.html'),
-        }
-      }
+    base: '/JavaScriptForStudents/'
     }
-  }
+    
